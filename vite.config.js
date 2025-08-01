1fbec3a5fa09ab4e54d1904e106a2ab8ad030bd9
akhilcoder7733 <postboxxx7@gmail.com>
2025-08-01 23:41:39 +0530

    sec commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/cyber-hacker-terminal/', // 👈 your repo name here with slashes
+  base: '/cyber-hacker-terminal/', // must include trailing slash
   plugins: [react()],
 });
