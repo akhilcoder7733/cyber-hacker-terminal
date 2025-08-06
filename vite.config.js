6a48cb44db4c17c327c21684d6b80b6cbf0914ec
akhilcoder7733 <postboxxx7@gmail.com>
2025-08-06 13:37:16 +0530

    Fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/cyber-hacker-terminal/', // must include trailing slash
   plugins: [react()],
+  base: '/', // must include trailing slash
 });
